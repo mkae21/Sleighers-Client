eeb97f9acaa6b15f1434b690a366e738b70c7551
정종문 <whdans4005@naver.com>
2024-04-30 00:53:01 +0900

    Modify: LSJ 작업 내용 병합

--- Assets/Scripts/System/InputManager.cs
+++ Assets/Scripts/System/InputManager.cs
@@ -1,14 +1,17 @@
 using UnityEngine;
 using Reader;
+using Protocol;
 
 public class InputManager : MonoBehaviour    
 {
 #region PrivateVariables
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
 #endregion
 
 #region PublicVariables
     static public InputManager instance;
-    public int playerIndex;
+    public int playerId;
 #endregion
 
 #region PrivateMethod
@@ -23,20 +26,28 @@ public class InputManager : MonoBehaviour
 
     private void PlayerInput()
     {
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis(HORIZONTAL);
+        float v = Input.GetAxis(VERTICAL);
+        //isDrifting = Input.GetKey(KeyCode.LeftShift);
+
+        if(Input.GetButton("Jump"))
+            WorldManager.instance.GetMyPlayerFromId(playerId).isBraking = true;
+
+        if(!Input.GetButton("Jump"))
+            WorldManager.instance.GetMyPlayerFromId(playerId).isBraking = false;
+
         Vector3 moveVector = new Vector3(h, 0, v);
         moveVector = Vector3.Normalize(moveVector);
+        PlayerMoveMessage playerMoveMessage = new PlayerMoveMessage(playerId, moveVector);
 
         byte[] data = new byte[1024];
         ByteWriter bw = new ByteWriter(data);
-        bw.WriteInt((int)Protocol.Type.c_PlayerPosition);
-        bw.WriteInt(playerIndex);
-        bw.WriteVector3(moveVector);
-        
+        bw.WriteBytes(bw.PlayerMoveMessageToByte(playerMoveMessage));
+    
         WorldManager.instance.GetMyPlayer().SetMoveVector(moveVector);
-        ServerManager.Instance().SendMessage(data);
+        // ServerManager.Instance().SendMessage(data);
     }
+
 #endregion
 
 #region PublicMethod
