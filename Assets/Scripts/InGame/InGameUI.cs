70f4d293db12169e717605e83c86db20b2c65284
Habin Choi <154362206+haaaabin@users.noreply.github.com>
2024-05-01 21:13:13 +0900

    Feat: InGame UI 타이머, 카운트 다운 , 속도계 UI 추가
    1. InGameUI에서 타이머(UpdateTimer()), 카운트 다운(SetCountDown()) 스크립트 작성
    2. InGameUI에서 속도계 표시(UpdateSpeedometer()) 테스트를 못해봐서 주석 처리
    3. Player에서 속도 표시를 위한 플레이어의 rigidbody 속력 반환 함수(GetSpeed()) 마찬가지로 주석 처리

---
+++ Assets/Scripts/InGame/InGameUI.cs
@@ -0,0 +1,72 @@
+using TMPro;
+using UnityEngine;
+
+public class InGameUI : MonoBehaviour
+{
+#region PublicVariables
+    public static InGameUI instance;
+
+    public TextMeshProUGUI text_Timer;
+    public TextMeshProUGUI text_CountDown;
+    public TextMeshProUGUI text_speedLabel;
+    public RectTransform arrow;
+
+    public float countDownDuration = 3.0f;
+    public float maxSpeed = 0.0f;
+    public float speed = 0.0f;
+    public float minSpeedArrowAngle;
+    public float maxSpeedArrowAngle;
+#endregion
+
+#region PrivateVariables
+    private float timer = 0.0f;
+#endregion
+
+#region PrivateMethod
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Update()
+    {
+        if(countDownDuration > 0.0f)
+            SetCountDown();
+        else
+            UpdateTimer();
+
+    }
+
+    private void UpdateTimer()
+    {
+        timer += Time.deltaTime;
+        int hours = (int)(timer / 3600);
+        int minutes = (int)(timer / 60 % 60);
+        int seconds = (int)(timer % 60);
+        text_Timer.text = string.Format("Time: {0:D2} : {1:D2} : {2:D2}", hours, minutes, seconds);
+    }
+
+    private void SetCountDown()
+    {
+        countDownDuration -= Time.deltaTime;
+        text_CountDown.text = ((int)countDownDuration + 1).ToString();
+        if(countDownDuration <= 0.0f)
+        {
+            text_CountDown.text = "GO!";
+            Destroy(text_CountDown.gameObject, 0.4f);          
+        }
+    }
+
+#endregion
+
+#region PublicMethod
+
+    // public void UpdateSpeedometer()
+    // {
+    //     vehicleSpeed = WorldManager.instance.GetMyPlayer().GetSpeed() * 3.6f;
+    //     desiredPosition = startPosition - endPosition;
+    //     float temp = vehicleSpeed / 240;
+    //     arrow.transform.eulerAngles = new Vector3(0, 0, (startPosition- temp * desiredPosition));
+    // }
+#endregion
+}
