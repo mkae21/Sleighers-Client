e921f4354d3261380b15f50082ccc82d7b7889c8
mkae21 <bbuchu123@gmail.com>
2024-05-10 19:28:57 +0900

    Add : InGame Scene 에 MapObject 추가
    Prototype Map Object

---
+++ Assets/TerrainMountainRoadMaker/Core/TerrainMountainRoadMaker.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+
+namespace TerrainMountainRoadMaker
+{
+	[Serializable]
+	public struct BezierInfo
+	{
+		public string label;
+		public Vector3 startPoint;
+		public Vector3 startTangent;
+		public Vector3 endPoint;
+		public Vector3 endTangent;
+
+		public BezierInfo(string Label,Vector3 StartPoint,Vector3 EndPoint,Vector3 StartTangent,Vector3 EndTangent)
+		{
+			label = Label;
+			startPoint = StartPoint;
+			startTangent = StartTangent;
+			endPoint = EndPoint;
+			endTangent = EndTangent;
+		}
+	}
+
+	public class TerrainMountainRoadMaker : MonoBehaviour
+	{
+		#region variables
+		[Header("setting")]
+		public int roadWidth = 5;
+		public int roadResolution = 250;
+		public float roundingCorner = 1f;
+
+		[Header("visibility")]
+		public bool showLabel = true;
+		public bool showArrows = true;
+		public bool showSpheres = true;
+		public bool showLines = true;
+
+		[Header("bezier info")]
+		public bool directInput;
+		public List<BezierInfo> Points = new List<BezierInfo>();
+
+		[Header("Undo & Redo")]
+		public List<float[,]> hightUndo = new List<float[,]>();
+		public List<float[,,]> alphaUndo = new List<float[,,]>();
+
+		[Header("Painting")]
+		public int splatIndex = -1;
+
+		[Header("hidden variables")]
+		[HideInInspector] public GUIStyle style = new GUIStyle();
+		[HideInInspector] public Terrain _terrain;
+		[HideInInspector] public TerrainCollider _collider;
+		#endregion
+
+
+		public Terrain terrain
+		{
+			get
+			{
+				if(_terrain==null)
+					_terrain = GetComponent<Terrain>();
+				return _terrain;
+			}
+		}
+		new public TerrainCollider collider
+		{
+			get
+			{
+				if(_collider==null)
+					_collider = GetComponent<TerrainCollider>();
+				return _collider;
+			}
+		}
+	}
+}
---
+++ Assets/TerrainMountainRoadMaker/Editor/Sticker2LayerEditor.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditorInternal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Sticker2Layer
+{
+	[CustomEditor(typeof(Sticker2Layer))]
+	public class Sticker2LayerEditor : Editor
+	{
+		#region CustomInspectorGUI
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			var t = (Sticker2Layer)target;
+
+			Undo.RecordObject(t, "Sticker2Layer");
+
+			if(GUILayout.Button("Go"))
+				Going(t);
+
+			if(t.undoList.Count>0)
+			{
+				EditorGUILayout.Space();
+				EditorGUILayout.BeginHorizontal();
+				{
+					if(GUILayout.Button("Undo"))
+					{
+						t.terrain.terrainData.SetHeightsDelayLOD(0,0,t.undoList[t.undoList.Count-1]);
+						t.undoList.RemoveAt(t.undoList.Count-1);
+					}
+					if(GUILayout.Button("Clear UndoLog"))
+						t.undoList.Clear();
+				}
+				EditorGUILayout.EndHorizontal();
+			}
+		}
+		#endregion
+
+		#region Change heights process
+		void Going(Sticker2Layer t)
+		{
+			var td = t.terrain.terrainData;
+			var hr = td.heightmapResolution;
+			var h = td.GetHeights(0,0,hr,hr);
+
+			var p = t.transform.position;
+			var fx = td.size.x / hr;
+			var fz = td.size.z / hr;
+
+			var mode = t.rayMode==RayMode.Up;
+			var fy = mode?0:td.heightmapResolution;
+				
+			t.undoList.Add((float[,])h.Clone());
+
+			for (var z=0;z<hr;z++)
+			{
+				for (var x=0;x<hr;x++)
+				{
+					var pp = new Vector3(x*fx,fy,z*fz);
+					var ray = new Ray(p+pp,(mode?Vector3.up:Vector3.down));
+					RaycastHit hit;
+
+					if(Physics.Raycast(ray,out hit,td.heightmapResolution,t.layer))
+					{
+						var y = hit.point.y - p.y - t.margin;
+						h[z,x] = (y / td.size.y);
+					}
+				}
+			}
+
+			td.SetHeightsDelayLOD(0,0,h);
+#if UNITY_2020_3_OR_NEWER
+			t.terrain.terrainData.SyncHeightmap();
+#else
+			t.terrain.ApplyDelayedHeightmapModification();
+#endif
+		}
+		#endregion
+	}
+}
