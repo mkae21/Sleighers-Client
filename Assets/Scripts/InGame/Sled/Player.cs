a221e31ea1733a67e5e2efc6909ab7ede37bc74b
mkae21 <bbuchu123@gmail.com>
2024-05-18 21:10:21 +0900

    Modify : Map수정 및 Ramp 보정 값 수정.
    장애물 및 점프 코스 추가, Ramp 보정 값 수정.

--- Assets/Scripts/InGame/Sled/Player.cs
+++ Assets/Scripts/InGame/Sled/Player.cs
@@ -155,8 +155,8 @@ public class Player : MonoBehaviour
             if(onRamp)
             {
                 // Ramp를 벗어날 때의 방향과 속도를 기반으로 힘을 가함
-                Vector3 launchDirection = sledModel.forward * 20f + sledModel.up * 30f;
-                sphere.AddForce(launchDirection.normalized * sphere.velocity.magnitude * 40f, ForceMode.Impulse);
+                Vector3 launchDirection = sledModel.forward * 15f + sledModel.up * 25f;
+                sphere.AddForce(launchDirection.normalized * sphere.velocity.magnitude * 55f, ForceMode.Impulse);
                 onRamp = false;
             }
         }
