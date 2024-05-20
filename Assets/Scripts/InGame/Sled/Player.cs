d65aa6fdd1434ca804e73cc62fd217db8eae0648
정종문 <whdans4005@naver.com>
2024-05-20 20:07:03 +0900

    Modify: 3으로 수정

--- Assets/Scripts/InGame/Sled/Player.cs
+++ Assets/Scripts/InGame/Sled/Player.cs
@@ -235,7 +235,7 @@ public class Player : MonoBehaviour
 
         if (toPosition != fromPosition)
         {
-            Vector3 extrapolatedPosition = toPosition + (toVelocity * latency * 6f);
+            Vector3 extrapolatedPosition = toPosition + (toVelocity * latency * 3f);
             sphere.transform.position = Vector3.Slerp(fromPosition, extrapolatedPosition, lerpAmount);
         }
 
