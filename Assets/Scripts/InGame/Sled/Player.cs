1a4b274dfd1ee257ded1a141d30930e16049efae
정종문 <whdans4005@naver.com>
2024-05-20 20:11:02 +0900

    Modify: 2.5로 변경

--- Assets/Scripts/InGame/Sled/Player.cs
+++ Assets/Scripts/InGame/Sled/Player.cs
@@ -235,7 +235,7 @@ public class Player : MonoBehaviour
 
         if (toPosition != fromPosition)
         {
-            Vector3 extrapolatedPosition = toPosition + (toVelocity * latency * 3f);
+            Vector3 extrapolatedPosition = toPosition + (toVelocity * latency * 2.5f);
             sphere.transform.position = Vector3.Slerp(fromPosition, extrapolatedPosition, lerpAmount);
         }
 
