8415e8fcbf74b3ccf8836baec7119d1dc9cfd536
Habin Choi <154362206+haaaabin@users.noreply.github.com>
2024-05-23 18:49:49 +0900

    Modify: 플레이어 startpoint 위치 수정, map2 obstacle 안사라지는 문제 해결

--- Assets/Scripts/InGame/ObstacleHolder.cs
+++ Assets/Scripts/InGame/ObstacleHolder.cs
@@ -10,6 +10,7 @@ public class ObstacleHolder : MonoBehaviour
 #region PublicVariables
     public static ObstacleHolder instance;
     public Transform obstacleHolder;
+    public Transform obstacleHolder2;
 #endregion
 
 #region PrivateVariables
@@ -27,12 +28,19 @@ public class ObstacleHolder : MonoBehaviour
     public void SetObstacles(Dictionary<string, Player> _players)
     {
         int totalObstacles = obstacleHolder.childCount;
+        int totalObstacles2 = obstacleHolder2.childCount;
         obstacles = new List<Obstacle>();
+        
         for (int i = 0; i < totalObstacles; i++)
         {
             obstacles.Add(obstacleHolder.GetChild(i).GetComponent<Obstacle>());
         }
 
+        for (int i= 0; i < totalObstacles2; i++)
+        {
+            obstacles.Add(obstacleHolder2.GetChild(i).GetComponent<Obstacle>());
+        }
+        
         foreach (KeyValuePair<string, Player> player in _players)
         {
             obstacles.Add(player.Value.GetComponentInChildren<Obstacle>());
@@ -57,7 +65,7 @@ public class ObstacleHolder : MonoBehaviour
         isPlayingSound = true;
         GameManager.Instance().soundManager.Play("Effect/Crash", SoundType.EFFECT);
 
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.7f);
         isPlayingSound = false;
     }
 #endregion
